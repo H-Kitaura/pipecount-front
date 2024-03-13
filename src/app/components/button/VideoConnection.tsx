b007eaaa5d65567c2001a265885c88cdd2a15984
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2024-03-13 16:33:06 +0900

    fix

--- src/app/components/button/VideoConnection.tsx
+++ src/app/components/button/VideoConnection.tsx
@@ -29,30 +29,31 @@ const VideoConnection = ({
       alert("カメラ認証ができませんでした。");
     }
   };
-  useEffect(() => {
-    const isLandscape = window.screen.orientation.type.includes("landscape");
-
-    const constraints = {
-      audio: false,
-      video: {
-        deviceId: getDevice ? getDevice.deviceId : undefined,
-        aspectRatio: isLandscape ? { ideal: 16 / 9 } : { ideal: 9 / 16 },
-        width: { ideal: isLandscape ? 1280 : 720 },
-        height: { ideal: isLandscape ? 720 : 1280 },
-      },
-    };
-
-    getPermission(constraints);
-    alert(selectedDevice);
-  }, []);
 
   const getDevice =
     devices &&
     selectedDevice &&
     devices.find((v: any) => v.deviceId === selectedDevice);
 
+  // useEffect(() => {
+  // const isLandscape = window.screen.orientation.type.includes("landscape");
+
+  const constraints = {
+    audio: false,
+    video: {
+      deviceId: getDevice ? getDevice.deviceId : undefined,
+      // aspectRatio: isLandscape ? { ideal: 16 / 9 } : { ideal: 9 / 16 },
+      // width: { ideal: isLandscape ? 1280 : 720 },
+      // height: { ideal: isLandscape ? 720 : 1280 },
+    },
+  };
+
+  // getPermission(constraints);
+  // alert(selectedDevice);
+  // }, []);
+
   const handleConnectClick = () => {
-    // getPermission(constraints);
+    getPermission(constraints);
     setCameraCheck(true);
   };
 
