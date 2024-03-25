37b1809eb4d40c30720523e3baec83fa01db06f9
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2024-03-26 08:40:45 +0900

    countResult

--- src/app/components/CountResult.tsx
+++ src/app/components/CountResult.tsx
@@ -4,21 +4,22 @@ type Props = {
 };
 const CountResult = ({ totalCounts }: Props) => {
   return (
-    <div>
-      <div className="my-4 px-8">
-        <p className="mb-2 text-lg font-semibold">過去のカウント</p>
-        <div className="grid grid-cols-3 gap-4">
-          {totalCounts.map((count, index) => (
-            <p key={index} className="text-4xl font-bold leading-none">
-              {index + 1}
-              {"=>"}
-              {count}
+    <div className="p-4">
+      <p className="text-center text-sm font-semibold mb-4">過去のカウント</p>
+      <div className="grid grid-cols-2 gap-2 sm:grid-cols-3">
+        {totalCounts.map((count, index) => (
+          <div
+            key={index}
+            className="flex justify-between items-center bg-white p-2 rounded-lg shadow hover:bg-gray-50"
+          >
+            <p className="text-xs font-medium text-gray-800">
+              {index + 1}回目:
             </p>
-          ))}
-        </div>
+            <p className="text-lg font-bold text-gray-600">{count}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
-
 export default CountResult;
