346a3f359a7dcd5a9a9c147c0b4c7b578f59ff4c
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2024-03-25 13:26:26 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    if (selectedDevice) {
+    if (selectedDevice && !cameraCheck) {
       getStream();
     }
   }, [selectedDevice, devices]);
