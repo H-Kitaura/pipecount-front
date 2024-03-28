47f3f42036b5988700a8ca918c744e9fe395e0d4
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2024-03-28 13:44:58 +0900

    fix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = ({
   setFeedBack,
 }: Props) => {
   return (
-    <div className="flex items-center justify-center w-full px-4 pb-12 space-x-4 pt-6">
+    <div className="flex items-center justify-center w-full px-2 pb-12 gap-2 pt-6">
       <PhotoButton
         videoRef={videoRef}
         mode={mode}
